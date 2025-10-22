1d465f243f3b1e14800b9f13cbadbaef6ef3f9de
samrawitshewakena3-gif <samrawitshewakena3@gmail.com>
2025-10-22 05:03:40 -0700

    Update solution.py

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,11 @@
-# your code here
+#problem 1
+name= input(" what is your name? ")
+favorite_food= input(" what is your favorite food ")
+current_year=input(" what is the cuurent year? ")
+age= input(" how old are you ")
+birth_year=int(current_year)-int(age)
+print ("birth year is : " + str(birth_year))
+
+print(f"hello {name}")
+print(f"it is great to meet you ")
+print(f"your favorite food is {favorite_food} and you are {age} years old")
